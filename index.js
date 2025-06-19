6df86d6e8191d94029fee0473fc397be1157c1a6
jimmy fresh <guns4kids@go-away.local>
2025-06-19 15:08:41 +0100

    add point popUps

--- index.js
+++ index.js
@@ -47,4 +47,60 @@ import { moveItem, getSelf } from "@esri/arcgis-rest-portal";
     console.log(`\nItem moved ${JSON.stringify(moved)}`);
 })();
 
-console.log("success");
+function infoLayer(Map, MapView, FeatureLayer) {
+    const map = new Map({
+        basemap: "streets-navigation-vector", // Use a valid ArcGIS basemap
+    });
+
+    const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-2, 54],
+        zoom: 6,
+    });
+
+    const featureLayer = new FeatureLayer({
+        url: "https://services-eu1.arcgis.com/OMdMOtfhATJPcHe3/arcgis/rest/services/NEH_outlets_PROD/FeatureServer/0",
+        outFields: [
+            "statusStart",
+            "latestEventStart",
+            "latestEventEnd",
+            "receivingWaterCourse",
+            "lastUpdated",
+            "ID",
+            "status",
+            "longitude",
+            "latitude",
+        ],
+        popupTemplate: {
+            title: "Outlet ID: {ID}",
+            content: `
+                <b>Status:</b> {status}<br>
+                <b>Status Start:</b> {statusStart}<br>
+                <b>Latest Event Start:</b> {latestEventStart}<br>
+                <b>Latest Event End:</b> {latestEventEnd}<br>
+                <b>Receiving Water Course:</b> {receivingWaterCourse}<br>
+                <b>Last Updated:</b> {lastUpdated}<br>
+                <b>Longitude:</b> {longitude}<br>
+                <b>Latitude:</b> {latitude}
+            `,
+        },
+    });
+    map.add(featureLayer);
+
+    // Event listener for clicking a feature
+    view.on("click", function (event) {
+        view.hitTest(event).then(function (response) {
+            const results = response.results;
+            if (results.length > 0) {
+                const graphic = results.filter(
+                    (result) => result.graphic.layer === featureLayer
+                )[0].graphic;
+                view.popup.open({
+                    features: [graphic],
+                    location: event.mapPoint,
+                });
+            }
+        });
+    });
+}
